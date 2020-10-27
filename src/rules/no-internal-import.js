8adfe535b4eb3750ee0a3e8faeda9c5ee77c89ce
Ivan Shmidt <bahoc47@gmail.com>
2020-10-27 21:13:23 +0100

    feat: consider files field from package.json (#12)
    Co-authored-by: blam <benjaminl@spotify.com>

--- src/rules/no-internal-import.js
+++ src/rules/no-internal-import.js
@@ -3,23 +3,74 @@ import moduleVisitor, {
 } from 'eslint-module-utils/moduleVisitor';
 import parse from 'parse-package-name';
 import getPackages from 'get-monorepo-packages';
+import path from 'path';
+import minimatch from 'minimatch';
+import fs from 'fs';
 
 export const meta = {
   schema: [makeOptionsSchema({})],
 };
 
+const withoutExtension = (importFile, fileEntry) => {
+  const importExt = path.extname(importFile);
+  if (importExt !== '') return [importFile, fileEntry];
+
+  const fileEntryExt = path.extname(fileEntry);
+  const newFileEntry =
+    fileEntryExt !== ''
+      ? fileEntry.replace(new RegExp(`\\${fileEntryExt}$`), '')
+      : fileEntry;
+  return [importFile, newFileEntry];
+};
+
 export const create = context => {
-  const { options: [moduleUtilOptions] } = context;
+  const {
+    options: [moduleUtilOptions],
+  } = context;
   const packages = getPackages(process.cwd());
 
   return moduleVisitor(node => {
     const { name, path: internalPath } = tryParse(node.value);
-    if (internalPath && packages.find(pkg => pkg.package.name === name)) {
-      context.report({
-        node,
-        message: `Import for monorepo package '${name}' is internal.`,
+    const matchedPackage = packages.find(pkg => pkg.package.name === name);
+    const packageRoot = matchedPackage.location;
+
+    // Need to take care of "files" field, since they are
+    // supposed to be part of the public API of the package
+    const absolutePathsForFiles =
+      matchedPackage.package.files &&
+      matchedPackage.package.files.map(file => {
+        const fileOrDirOrGlob = path.join(packageRoot, file);
+
+        try {
+          if (fs.lstatSync(fileOrDirOrGlob).isDirectory()) {
+            return path.join(fileOrDirOrGlob, '**', '*');
+          }
+          return fileOrDirOrGlob;
+        } catch (e) {
+          return fileOrDirOrGlob;
+        }
       });
+    const absoluteInternalPath = path.join(packageRoot, internalPath);
+
+    if (!internalPath) return;
+    if (!matchedPackage) return;
+    if (absolutePathsForFiles) {
+      const isImportWithinFiles = absolutePathsForFiles.some(maybeGlob => {
+        // If import doesn't have an extension, strip it from the file entry
+        const [theImport, theFileEntry] = withoutExtension(
+          absoluteInternalPath,
+          maybeGlob
+        );
+        return minimatch(theImport, theFileEntry);
+      });
+
+      if (isImportWithinFiles) return;
     }
+
+    context.report({
+      node,
+      message: `Import for monorepo package '${name}' is internal.`,
+    });
   }, moduleUtilOptions);
 };
 
--- test/rules/no-internal-import.test.js
+++ test/rules/no-internal-import.test.js
@@ -3,7 +3,10 @@ import path from 'path';
 import * as monorepo from '../../src';
 
 const RULE = 'no-internal-import';
-const ERROR = { message: `Import for monorepo package 'foo' is internal.` };
+const ERROR = packageName => ({
+  message: `Import for monorepo package '${packageName}' is internal.`,
+});
+
 const fixtures = path.join(__dirname, '../fixture/yarn');
 
 process.cwd = jest.fn(() => fixtures);
@@ -18,13 +21,39 @@ ruleTester.run(RULE, monorepo.rules[RULE], {
       code: `import { pkg } from 'bar'`,
       filename: path.join(fixtures, 'packages/bar/index.js'),
     },
+    {
+      code: `import { pkg } from 'bar/allowed'`,
+      filename: path.join(fixtures, 'packages/bar/index.js'),
+    },
+    {
+      code: `import { pkg } from 'bar/nested/allowedToo'`,
+      filename: path.join(fixtures, 'packages/bar/index.js'),
+    },
+    {
+      code: `import { pkg } from 'bar/nested/this/is/also/allowed'`,
+      filename: path.join(fixtures, 'packages/bar/index.js'),
+    },
+    {
+      code: `import { pkg } from 'bar/complex/glob/anything/goes/here/test.ts'`,
+      filename: path.join(fixtures, 'packages/bar/index.js'),
+    },
   ],
 
   invalid: [
     {
       code: `import pkg from 'foo/src/pkg'`,
       filename: path.join(fixtures, 'packages/bar/index.js'),
-      errors: [ERROR],
+      errors: [ERROR('foo')],
+    },
+    {
+      code: `import { pkg } from 'bar/allowedNot'`,
+      filename: path.join(fixtures, 'packages/bar/index.js'),
+      errors: [ERROR('bar')],
+    },
+    {
+      code: `import { pkg } from 'bar/complex/glob/anything/goes/here/test.tsx'`,
+      filename: path.join(fixtures, 'packages/bar/index.js'),
+      errors: [ERROR('bar')],
     },
   ],
 });
